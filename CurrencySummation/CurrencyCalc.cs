eeb39be50cd0930c1efd8146ee240921468fa016
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-22 15:07:43 +0100

    Add project files.

---
+++ CurrencySummation/CurrencyCalc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencySummation
+{
+    public class CurrencyCalc
+    {
+        private string _transactions;
+        private string[] _transactionArray;
+
+        public CurrencyCalc(string transactions) 
+        {
+            _transactions = transactions;
+        }
+
+        public void ReplaceWhiteSpaces()
+        {
+            _transactions = _transactions.Replace(" ", "");
+        }
+
+        public void ReplaceKWithThousand()
+        {
+            _transactions = _transactions.Replace("k", "000");
+        }
+
+        public void ConvertToArray()
+        {
+            _transactionArray = _transactions.Split(',');
+        }
+
+        public int SumDollarTransactions() // "24$, 59$, 60k£, 20£, 5k$"
+        {
+            int intSum = 0;
+            for (int i = 0; i < _transactionArray.Length; i++)
+            {
+                string sum = _transactionArray[i];
+                for(int j = sum.Length - 1; j >= 0; j--)
+                {
+                    char c = sum[j];
+                    if (c == '$')
+                    {
+                        string trimmedSum = sum.Remove(sum.Length - 1);
+                        int x = Int32.Parse(trimmedSum);
+                        intSum += x;
+                    }
+                }
+            }
+            return intSum;
+        }
+    }
+}
---
+++ CurrencySummation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CurrencySummation
+{
+    class Program
+    {
+        // https://edabit.com/challenge/RrPW2X9Bkbkr4FLWD
+        // I misread the assignment: currency symbols occur at the end, which led
+        // to an inverse loop..
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            CurrencyCalc sum = new("24$, 59$, 60k£, 20£, 5k$"); // -> ( 24 + 59 + 5000 ) = 5083 $
+            sum.ReplaceWhiteSpaces();
+            sum.ReplaceKWithThousand();
+            sum.ConvertToArray();
+            Console.Write(sum.SumToList());
+        }
+    }
+}
